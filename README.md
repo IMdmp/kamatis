3ca189645ad03cd579d50454614d79f11cc59c08
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2019-07-04 21:10:54 +0800

    initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Video capture library in the works
 
 based on <b> Grafika test app </b> read their readme: https://github.com/google/grafika to find out more on Android Graphics.
+
+
+
+useful later on : https://stackoverflow.com/questions/18558077/fully-change-package-name-including-company-domain
---
+++ app/src/main/java/com/dmp/project/kamatis/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dmp.project.kamatis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
